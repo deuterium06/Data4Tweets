6d04338cfd5fd46ec8a56dd2d1e1497234adb50d
deuterium06 <57389422+deuterium06@users.noreply.github.com>
2023-05-23 12:34:55 +0800

    Change from pytest to unittest (#2)
    Minor fixes in CircleCI config

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,17 @@
 import pytest
 import json
+import unittest
 import sys, os
 sys.path.append('../')
 
-from Data4Tweets.app import app
+from app import app
 
-def test_index():
-    response = app.test_client().get('/')
-    assert response.status_code == 200
+class Test(unittest.TestCase):
+
+    def test_index(self):
+        
+        response = app.test_client().get('/')
+        self.assertEqual(response.status_code, 200, "Status should be 200.")
+
+if __name__ == '__main__':
+    unittest.main()
